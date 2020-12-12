6f65a589d3af1c6bac095292191e86caf958fb8c
Arthur Fedotiev <artur.fedotiew@gmail.com>
2020-12-12 20:31:31 +0200

    US011: modified heroes list page with else block when no seelcted hero

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../shared/services/heroes.service';
 
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
   styleUrls: ['./user-page.component.css'],
 })
-export class UserPageComponent {}
+export class UserPageComponent implements OnInit {
+  showList: boolean;
+
+  constructor(private heroesService: HeroesService) {}
+
+  ngOnInit() {
+    this.showList = !!this.heroesService.getSelectedHeroes().length;
+  }
+}
